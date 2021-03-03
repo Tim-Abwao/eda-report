ed9209a9e6e822aef72a59a040493c3a70fba1bd
Tim-Abwao <abwaomusungu@gmail.com>
2021-03-03 14:37:27 +0300

    Add .csv and .xlsx file import

---
+++ eda_report/read_file.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+def df_from_file(filename):
+    """Load the file `filename` as a pandas DataFrame."""
+    file_extension = filename.rsplit('.')[-1]
+
+    if file_extension == 'csv':
+        return pd.read_csv(filename)
+    elif file_extension == 'xlsx':
+        return pd.read_excel(filename, engine='openpyxl')
