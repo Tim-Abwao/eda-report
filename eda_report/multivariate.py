9e26ec0c8a6f600ce1a05a55db80fe1bab61675e
Tim-Abwao <abwaomusungu@gmail.com>
2021-03-22 11:41:33 +0300

    Add function to display graphs in MultiVariable
    Add the show_correlation_heatmap and show_joint_heatmap methods to display graphs.
    Conform some variables and methods to PEP8.

--- eda_report/multivariate.py
+++ eda_report/multivariate.py
@@ -4,32 +4,60 @@ from itertools import combinations
 from eda_report.plotting import Fig, savefig
 from eda_report.validate import validate_input_dtype
 from tqdm import tqdm
+from PIL import Image
 
 
 class MultiVariable:
-    """The blueprint for containers to hold data with multiple columns
-    (features).
+    """The blueprint for containers to hold the properties of data with
+    *multiple columns/features*.
+
+    The input data is expected to be a ``pandas.DataFrame``. If the data is of
+    any other type, then there'll be an attempt to explicitly convert it a
+    ``DataFrame``. If this fails, an :class:`~eda_report.exceptions.InputError`
+    is raised.
     """
 
     def __init__(self, data, graph_color='orangered'):
         """Initialise an instance of :class:`MultiVariable`.
 
         :param data: The data to process, ideally a ``pandas.DataFrame``
-            with mutliple columns.
+            with several columns.
         :type data: An array-like, sequence, iterable or dict
         :param graph_color: The color to apply to the graphs created,
             defaults to 'orangered'.
         :type graph_color: str, optional
         """
         self.data = validate_input_dtype(data)
-        self.GRAPH_COLOR = graph_color
-        #: A ``DataFrame`` of the numeric columns present in the data
+        #: The color applied to the created graphs.
+        self.graph_color = graph_color
+        #: A ``DataFrame`` of all the numeric columns/features.
         self.numeric_cols = self._select_cols('number')
-        #: A ``DataFrame`` of the categorical columns present in the data
+        #: A ``DataFrame`` of all the categorical columns/features. Please
+        #: note that boolean features are also considered categorical here.
         self.categotical_cols = self._select_cols('object', 'bool')
-        #: A ``DataFrame`` of correlation coefficients for the numeric columns
+        #: A ``DataFrame`` of Pearson correlation coefficients for the numeric
+        #: columns/features.
         self.correlation_df = self._get_correlation()
         self._get_bivariate_analysis()
+
+    def show_correlation_heatmap(self):
+        """Display a heatmap of Pearson correlation coefficients for all
+        numeric columns/features present.
+        """
+        if hasattr(self, 'joint_correlation_plot'):
+            image = Image.open(self.joint_correlation_plot)
+            image.show()
+        else:
+            print('Not enough numeric variables to compare.')
+
+    def show_joint_scatterplot(self):
+        """Display a joint scatterplot of all the numeric columns/features.
+        """
+        if hasattr(self, 'joint_scatterplot'):
+            image = Image.open(self.joint_scatterplot)
+            image.show()
+        else:
+            print('Not enough numeric variables to compare.')
 
     def _get_bivariate_analysis(self):
         """Compare numeric variable pairs.
@@ -38,6 +66,8 @@ class MultiVariable:
             self._plot_joint_scatterplot()
             self._plot_joint_correlation()
             self._compare_variable_pairs()
+        else:
+            print('Not enough numeric variables to compare.')
 
     def _select_cols(self, *dtypes):
         """Get a DataFrame of the numeric columns present.
@@ -56,8 +86,8 @@ class MultiVariable:
         """
         fig = sns.pairplot(
             self.numeric_cols, height=1.75,
-            plot_kws={'color': self.GRAPH_COLOR},
-            diag_kws={'color': self.GRAPH_COLOR}
+            plot_kws={'color': self.graph_color},
+            diag_kws={'color': self.graph_color}
         )
         fig.fig.suptitle('Scatter-plots of Numeric Columns', x=0.5, y=1.04,
                          size=20)
@@ -69,7 +99,7 @@ class MultiVariable:
         ax = fig.subplots()
         sns.heatmap(self.correlation_df, annot=True, yticklabels=True,
                     mask=np.triu(self.correlation_df), ax=ax,
-                    cmap=sns.light_palette(self.GRAPH_COLOR, as_cmap=True))
+                    cmap=sns.light_palette(self.graph_color, as_cmap=True))
         ax.tick_params(rotation=45)
         fig.suptitle('Correlation in Numeric Columns', size=15)
 
@@ -96,9 +126,9 @@ class MultiVariable:
         fig = Fig(figsize=(8.2, 4))
         ax1, ax2 = fig.subplots(1, 2)
         sns.regplot(x=var1, y=var2, data=self.data, ax=ax1, truncate=False,
-                    color=self.GRAPH_COLOR)
+                    color=self.graph_color)
         sns.regplot(x=var2, y=var1, data=self.data, ax=ax2, truncate=False,
-                    color=self.GRAPH_COLOR)
+                    color=self.graph_color)
         ax1.set_title(f'Scatterplot - {var1} vs {var2}'.title(), size=9)
         ax2.set_title(f'Scatterplot - {var2} vs {var1}'.title(), size=9)
 
