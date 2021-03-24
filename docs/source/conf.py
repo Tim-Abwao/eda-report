20ccb3447b9337f9c9dd4092379ef3d5f6f44c8d
Tim-Abwao <abwaomusungu@gmail.com>
2021-03-25 02:56:37 +0300

    Update python path in docs/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../eda_report'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 
 # -- Project information -----------------------------------------------------
