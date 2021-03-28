b375e4c8308a9a44fd01fbf7b324c774c819ae3f
Tim-Abwao <abwaomusungu@gmail.com>
2021-03-28 18:49:33 +0300

    Bump version to 1.1.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Abwao'
 author = 'Abwao'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.2'
+release = '1.1.3'
 
 
 # -- General configuration ---------------------------------------------------
