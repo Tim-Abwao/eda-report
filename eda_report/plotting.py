95524fb014ca333117caf517660d208d6742c0ec
Tim-Abwao <abwaomusungu@gmail.com>
2021-03-28 15:58:27 +0300

    Update graph background color
    Set background color to white. The transparent background was rendered as black in some image-viewing programs, hampering visibility

--- eda_report/plotting.py
+++ eda_report/plotting.py
@@ -3,15 +3,18 @@ from io import BytesIO
 import matplotlib
 from matplotlib.figure import Figure
 
+
 # Matplotlib configuration
 matplotlib.rcParams['figure.dpi'] = 150  # resolution (dots per inch)
 matplotlib.rcParams['figure.autolayout'] = True  # tight layout
-matplotlib.rcParams['savefig.transparent'] = False   # transparent background
-matplotlib.rcParams["savefig.edgecolor"] = 'k'  # black frame
+matplotlib.rcParams['savefig.transparent'] = False  # transparent background
+matplotlib.rcParams['savefig.edgecolor'] = 'k'  # black frame
+matplotlib.rcParams['savefig.facecolor'] = 'w'  # white background
 matplotlib.rcParams['font.family'] = 'serif'
 matplotlib.rcParams['axes.spines.top'] = False  # remove top spine
 matplotlib.rcParams['axes.spines.right'] = False  # remove left spine
 matplotlib.use('agg')  # use non-interactive matplotlib back-end
+
 
 Fig = Figure
 
