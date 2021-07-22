02121a75f88da3b2f2f0b04a2a314c7e7ab54bfc
Tim-Abwao <abwaomusungu@gmail.com>
2021-07-22 23:25:59 +0300

    Update univariate module
    Switch to the numpy docstring format. Update docs.
    Give variables and functions intuitive names.
    Allow renaming of Variable instances.
    Update the string representation of Variable instances.

--- eda_report/univariate.py
+++ eda_report/univariate.py
@@ -1,145 +1,190 @@
+from collections.abc import Iterable
 from textwrap import shorten
 
-from pandas.api.types import (
-    is_bool_dtype,
-    is_datetime64_any_dtype,
-    is_numeric_dtype,
-)
+from pandas import DataFrame
+from pandas.api.types import (is_bool_dtype, is_datetime64_any_dtype,
+                              is_numeric_dtype)
 
 from eda_report.validate import validate_univariate_input
 
 
 class Variable:
-    """This is the blueprint for objects that analyse and plot one-dimensional
-    datasets: a *single column/feature*.
+    """This defines objects for analysing one-dimensional datasets — a single
+    column.
+
+    The input data is internally handled as a :class:`~pandas.Series` in order
+    to leverage pandas_ built-in statistical methods, as well as functions
+    from the `SciPy ecosystem`_.
+
+    .. _pandas: https://pandas.pydata.org/
+    .. _SciPy ecosystem: https://www.scipy.org/
+
+    The result is a concise summary of the data's statistical properties.
+
+    Parameters
+    ----------
+    data : Iterable
+        The data to analyse.
+    name : Optional[str]
+        The name to assign the ``Variable``, by default None.
+
+    Example
+    --------
+    >>> Variable(data=range(20), name="0 to 20")
+                    Overview
+                    ========
+    Name: 0 to 20
+    Type: numeric
+    Unique Values: 20 -> {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, [...]
+    Missing Values: None
+                    ***
+            Summary Statistics
+                            0 to 20
+    Number of observations  20.00000
+    Average                  9.50000
+    Standard Deviation       5.91608
+    Minimum                  0.00000
+    Lower Quartile           4.75000
+    Median                   9.50000
+    Upper Quartile          14.25000
+    Maximum                 19.00000
+    Skewness                 0.00000
+    Kurtosis                -1.20000
     """
 
-    def __init__(
-        self, data, *, name=None, target_data=None
-    ):
-        """Initialise an instance of :class:`Variable`.
-
-        :param data: The data to process.
-        :type data: array-like, sequence, iterable.
-        :param graph_color: The color to apply to the graphs created,
-            defaults to 'orangered'.
-        :type graph_color: str, optional
-        :param name: The feature's name.
-        :type name: str, optional
-        :param target_data: Data for the target variable (dependent
-            feature). Currently used to group and color-code values in graphs.
-        :type target_data: array-like, optional
+    def __init__(self, data: Iterable, *, name: str = None) -> None:
+        """Initialise an instance of :class:`~eda_report.univariate.Variable`.
         """
         self.data = validate_univariate_input(data)
-        #: The *name* of the *column/feature*. If unspecified in the ``name``
-        #: argument during instantiation, this will be taken as the value of
-        #: the ``name`` attribute of the input data.
-        self.name = self._get_name(name)
-        #: The *type* of feature; either *boolean*, *categorical*, *datetime*
-        #:  or *numeric*.
+
+        #: The ``Variable``'s *name*. If no name was specified during
+        #: instantiation, the name will be equal to the value of the ``name``
+        #: attribute of the input data (if present) or zero.
+        self.name = name or self.data.name
+
+        #: The ``Variable``'s *type* — one of *boolean*, *categorical*,
+        #: *datetime* or *numeric*.
         self.var_type = self._get_variable_type()
-        #: *Summary statistics* for the *column/feature*, as a
-        #: :class:`pandas.DataFrame`.
+
+        #: *Summary statistics* as a :class:`~pandas.DataFrame`.
         self.statistics = self._get_summary_statistics()
-        #: The *number of unique values* present in the *column/feature*.
+
+        #: The *number of unique values* present in the ``Variable``.
         self.num_unique = self.data.nunique()
-        #: The set of *unique values* present in the *column/feature*.
+
+        #: The *set of unique values* present in the ``Variable``.
         self.unique = set(self.data.unique())
-        #: The number of *missing values*.
-        self.missing = self._get_missing_values()
-        self.TARGET_DATA = validate_univariate_input(target_data)
 
-    def __repr__(self):
-        """Creates the string representation for :class:`Variable` objects."""
-        return f"""\
-            Overview
-            ========
-Name: {self.name},
-Type: {self.var_type},
-Unique Values: {shorten(f'{self.num_unique} -> {self.unique}', 60)},
-Missing Values: {self.missing}
+        #: *Missing value information* in the form ``number (percentage%)``.
+        self.missing = self._get_missing_values_info()
 
-        Summary Statistics
-        ==================
-{self.statistics}
-"""
+    def __repr__(self) -> str:
+        """Get the string representation of the ``Variable``.
 
-    def _get_name(self, name=None):
-        """Set the feature's name.
-
-        :param name: The name to give the feature, defaults to None
-        :type name: str, optional
+        Returns
+        -------
+        str
+            A summary of the ``Variable``'s properties.
         """
-        if name:
-            self.data = self.data.rename(name)
+        return "\n".join(
+            [
+                "\t\tOverview",
+                "\t\t========",
+                f"Name: {self.name}",
+                f"Type: {self.var_type}",
+                "Unique Values: "
+                + f"{shorten(f'{self.num_unique} -> {self.unique}', 60)}",
+                f"Missing Values: {self.missing}",
+                "\t\t  ***",
+                "\t  Summary Statistics",
+                f"{self.statistics}",
+            ]
+        )
 
-        return self.data.name
+    def rename(self, name: str = None):
+        """Rename the ``Variable`` as specified.
 
-    def _get_variable_type(self):
-        """Get the variable type: 'categorical' or 'numeric'."""
+        Parameters
+        ----------
+        name
+            The name to assign to the ``Variable``, by default None.
+        """
+        if name is not None:
+            self.name = self.data.name = name
+            self.statistics.set_axis([name], axis=1, inplace=True)
+            if hasattr(self, "most_common_items"):
+                self.most_common_items.set_axis([name], axis=1, inplace=True)
+
+    def _get_variable_type(self) -> str:
+        """Determine the ``Variable``'s type.
+
+        Returns
+        -------
+        var_type : {"boolean", "categorical", "datetime", "numeric"}
+        """
         if is_numeric_dtype(self.data):
             if is_bool_dtype(self.data) or set(self.data.dropna()) == {0, 1}:
                 return "boolean"
             else:
-                # Only int and float types
                 return "numeric"
         elif is_datetime64_any_dtype(self.data):
             self.data = self.data.dt.strftime("%c")
             return "datetime"
         else:
-            # Handle str, etc as categorical
+            # Handle object, string, etc as categorical
             return "categorical"
 
-    def _get_summary_statistics(self):
+    def _get_summary_statistics(self) -> DataFrame:
         """Get summary statistics for the column/feature."""
         if self.var_type == "numeric":
-            return self._numeric_summary_statictics()
+            summary = self.data.describe().set_axis(
+                [
+                    "Number of observations",
+                    "Average",
+                    "Standard Deviation",
+                    "Minimum",
+                    "Lower Quartile",
+                    "Median",
+                    "Upper Quartile",
+                    "Maximum",
+                ]
+            )
+            summary["Skewness"] = self.data.skew()
+            summary["Kurtosis"] = self.data.kurt()
+
+            return summary.round(7).to_frame()
+
         elif self.var_type in {"boolean", "categorical", "datetime"}:
             if (self.data.shape[0] / self.data.nunique()) > 1.5:
                 # If less than 2-thirds of the values are unique
                 self.data = self.data.astype("category")
             else:
                 self.data = self.data.astype("object")
-            return self._categorical_summary_statistics()
 
-    def _numeric_summary_statictics(self):
-        """Get summary statistics for a numeric column/feature."""
-        summary = self.data.describe()
-        summary.index = [
-            "Number of observations",
-            "Average",
-            "Standard Deviation",
-            "Minimum",
-            "Lower Quartile",
-            "Median",
-            "Upper Quartile",
-            "Maximum",
-        ]
-        summary["Skewness"] = self.data.skew()
-        summary["Kurtosis"] = self.data.kurt()
+            summary = self.data.describe()[["count", "unique", "top"]]
+            summary.index = [
+                "Number of observations",
+                "Unique values",
+                "Mode (Highest occurring value)",
+            ]
 
-        return summary.round(7).to_frame()
+            # Get most common items and their relative frequency (%)
+            most_common_items = self.data.value_counts().head()
+            n = len(self.data)
+            self.most_common_items = most_common_items.apply(
+                lambda x: f"{x} ({x / n:.2%})"
+            ).to_frame()
 
-    def _categorical_summary_statistics(self):
-        """Get summary statistics for a categorical column/feature."""
-        summary = self.data.describe()[["count", "unique", "top"]]
-        summary.index = [
-            "Number of observations",
-            "Unique values",
-            "Mode (Highest occurring value)",
-        ]
+            return summary.to_frame()
 
-        # Get most common items and their relative frequency (%)
-        most_common_items = self.data.value_counts().head()
-        n = len(self.data)
-        self.most_common_items = most_common_items.apply(
-            lambda x: f"{x} ({x / n:.2%})"
-        ).to_frame()
+    def _get_missing_values_info(self) -> str:
+        """Get the number of missing values in the ``Variable``.
 
-        return summary.to_frame()
-
-    def _get_missing_values(self):
-        """Get the number of missing values in the column/feature."""
+        Returns
+        -------
+        str
+            Details about the number of missing values.
+        """
         missing_values = self.data.isna().sum()
         if missing_values == 0:
             return "None"
