4902165fd626467c2dce4be50f474661a44cca6e
Tim-Abwao <abwaomusungu@gmail.com>
2021-07-27 02:24:43 +0300

    Switch to pytest
    Swap unittest for the faster, cleaner & easier-to-use pytest.
    Update tests to cover the changes made to respective modules.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,23 +1,18 @@
-import unittest
-from pathlib import Path
-
-import pandas as pd
+from pandas import DataFrame
 from eda_report.cli import process_cli_args
+import sys
 
 
-class TestCLIArgumentParsing(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        # Create a dummy input file
-        pd.DataFrame(range(50)).to_csv("test-data.csv", index=False)
+class TestCLIArgumentParsing:
+    def test_with_all_args(self, tmp_path):
 
-    @classmethod
-    def tearDownClass(cls):
-        Path("test-data.csv").unlink()  # Delete the dummy file
+        cli_test_dir = tmp_path / "cli-tests"
+        cli_test_dir.mkdir()
+        csv_file = cli_test_dir / "data.csv"
+        csv_file.write_text("1, 2, 3\n")
 
-    def test_with_all_args(self):
         args = process_cli_args(
-            "test-data.csv",
+            f"{csv_file}",
             "-o",
             "cli-test-1.docx",
             "-t",
@@ -28,17 +23,42 @@ class TestCLIArgumentParsing(unittest.TestCase):
             "0",
         )
         # Check whether the supplied arguments were set
-        self.assertIsInstance(args.infile, pd.DataFrame)
-        self.assertEqual(args.outfile, "cli-test-1.docx")
-        self.assertEqual(args.title, "CLI Test")
-        self.assertEqual(args.color, "teal")
-        self.assertEqual(args.target, "0")
+        assert isinstance(args.infile, DataFrame)
+        assert args.outfile == "cli-test-1.docx"
+        assert args.title == "CLI Test"
+        assert args.color == "teal"
+        assert args.target == "0"
 
-    def test_with_default_args(self):
-        args = process_cli_args("test-data.csv")
+    def test_with_default_args(self, tmp_path):
+
+        cli_test_dir = tmp_path / "cli-tests"
+        cli_test_dir.mkdir()
+        excel_file = cli_test_dir / "data.xlsx"
+        DataFrame([1, 2, 3]).to_excel(
+            excel_file, index=False, engine="openpyxl"
+        )
+
+        args = process_cli_args(f"{excel_file}")
+
         # Check if the default arguments were set
-        self.assertIsInstance(args.infile, pd.DataFrame)
-        self.assertEqual(args.outfile, "eda-report.docx")
-        self.assertEqual(args.title, "Exploratory Data Analysis Report")
-        self.assertEqual(args.color, "orangered")
-        self.assertEqual(args.target, None)
+        assert isinstance(args.infile, DataFrame)
+        assert args.outfile == "eda-report.docx"
+        assert args.title == "Exploratory Data Analysis Report"
+        assert args.color == "cyan"
+        assert args.target is None
+
+    def test_with_args_from_stdin(self, tmp_path, monkeypatch):
+
+        cli_test_dir = tmp_path / "cli-tests"
+        cli_test_dir.mkdir()
+        csv_file = cli_test_dir / "data.csv"
+        csv_file.write_text("1, 2, 3\n")
+
+        monkeypatch.setattr(sys, "argv", ["eda_cli", f"{csv_file}"])
+
+        args = process_cli_args()
+
+        assert args.outfile == "eda-report.docx"
+        assert args.title == "Exploratory Data Analysis Report"
+        assert args.color == "cyan"
+        assert args.target is None
--- tests/test_document_creation.py
+++ tests/test_document_creation.py
@@ -1,62 +1,85 @@
-import unittest
-from pathlib import Path
-
-import pandas as pd
-import seaborn as sns
-from docx import Document
-from eda_report import get_word_report
+from pandas.core.frame import DataFrame
+from seaborn import load_dataset
+from eda_report.document import ReportDocument
 
 
-class TestDocumentProperties(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.outfile = "test-report.docx"
+class TestReportWithIdealInput:
 
-    @classmethod
-    def tearDownClass(cls):
-        Path(cls.outfile).unlink()  # Delete the file
-        del cls.outfile
+    data = load_dataset("iris")
+    content = ReportDocument(
+        data,
+        title="Test Report",
+        graph_color="teal",
+        target_variable="species",
+    )
 
-    def test_report_creation_in_multivariate_mixed_type_data(self):
-        # Generate the report
-        get_word_report(
-            data=sns.load_dataset("iris"),
-            output_filename=self.outfile,
-            title="Mutlivariate",
+    def test_bivariate_analysis(self):
+        assert self.content.bivariate_summaries == {
+            ("sepal_length", "petal_length"): (
+                "Sepal_Length and Petal_Length have strong positive "
+                "correlation (0.87)."
+            ),
+            ("petal_length", "petal_width"): (
+                "Petal_Length and Petal_Width have very strong positive "
+                "correlation (0.96)."
+            ),
+            ("sepal_length", "sepal_width"): (
+                "Sepal_Length and Sepal_Width have very weak negative "
+                "correlation (-0.12)."
+            ),
+            ("sepal_length", "petal_width"): (
+                "Sepal_Length and Petal_Width have strong positive "
+                "correlation (0.82)."
+            ),
+            ("sepal_width", "petal_length"): (
+                "Sepal_Width and Petal_Length have weak negative correlation"
+                " (-0.43)."
+            ),
+            ("sepal_width", "petal_width"): (
+                "Sepal_Width and Petal_Width have weak negative correlation "
+                "(-0.37)."
+            ),
+        }
+
+    def test_graph_color(self):
+        assert self.content.GRAPH_COLOR == "teal"
+
+    def test_intro(self):
+        assert self.content.intro_text == (
+            "The dataset consists of 150 rows (observations) and 5 columns "
+            "(features), 4 of which are numeric."
         )
-        # Check if the report is actually created and saved
-        self.assertTrue(Path(self.outfile).is_file())
-        # Check if the title in the document is as stipulated
-        self.assertEqual(
-            Document(self.outfile).paragraphs[0].text, "Mutlivariate"
-        )
 
-    def test_report_creation_in_numeric_univariate_data(self):
-        # Generate the report
-        get_word_report(
-            data=pd.DataFrame(range(50)),
-            output_filename=self.outfile,
-            title="Numeric Univariate",
-        )
-        # Check if the report is actually created and saved
-        self.assertTrue(Path(self.outfile).is_file())
-        # Check if the title in the document is as stipulated
-        self.assertEqual(
-            Document(self.outfile).paragraphs[0].text,
-            "Numeric Univariate",
-        )
+    def test_bivariate_graphs(self):
+        assert "correlation_heatmap" in self.content.multivariate_graphs
+        assert "scatterplots" in self.content.multivariate_graphs
 
-    def test_report_creation_in_categorical_univariate_data(self):
-        # Generate the report
-        get_word_report(
-            data=pd.DataFrame(["only one item"]),
-            output_filename=self.outfile,
-            title="Categorical Univariate",
+    def test_target_data(self):
+        assert self.content.TARGET_VARIABLE.equals(self.data["species"])
+
+
+class TestReportWithLimitedInput:
+
+    data = DataFrame(
+        {"categorical": list("ABCDEFGHIJKL"), "numeric": range(12)}
+    )
+    report = ReportDocument(data)
+
+    def test_title(self):
+        assert self.report.TITLE == "Exploratory Data Analysis Report"
+
+
+class TestReportWithUnivariateInput:
+
+    univariate_numeric_report = ReportDocument(DataFrame(range(5)))
+    univariate_categorical_report = ReportDocument(DataFrame(["a"]))
+
+    def test_title(self):
+        assert (
+            self.univariate_numeric_report.TITLE
+            == "Exploratory Data Analysis Report"
         )
-        # Check if the report is actually created and saved
-        self.assertTrue(Path(self.outfile).is_file())
-        # Check if the title in the document is as stipulated
-        self.assertEqual(
-            Document(self.outfile).paragraphs[0].text,
-            "Categorical Univariate",
+        assert (
+            self.univariate_categorical_report.TITLE
+            == "Exploratory Data Analysis Report"
         )
