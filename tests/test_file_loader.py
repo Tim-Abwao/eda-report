1cdd9e552d460787b10007ff431804b8bb2e999d
Tim-Abwao <abwaomusungu@gmail.com>
2022-03-07 03:14:35 +0300

    Use tmp_path_factory for efficiency

--- tests/test_file_loader.py
+++ tests/test_file_loader.py
@@ -4,6 +4,11 @@ from eda_report.read_file import df_from_file
 from pandas import DataFrame
 
 
+@pytest.fixture(scope="session")
+def temp_data_dir(tmp_path_factory):
+    return tmp_path_factory.mktemp("data")
+
+
 class TestFileLoader:
 
     data = DataFrame(range(50), columns=["data"])
@@ -12,15 +17,15 @@ class TestFileLoader:
         # Check that when no filepath is specified, None is returned.
         assert df_from_file() is None
 
-    def test_csv_file_load(self, tmp_path):
+    def test_csv_file_load(self, temp_data_dir):
         # Check that a valid csv file is read as a DataFrame
-        csv_file = tmp_path / "data.csv"
+        csv_file = temp_data_dir / "data.csv"
         self.data.to_csv(csv_file, index=False)
         assert df_from_file(csv_file).equals(self.data)
 
-    def test_excel_file_load(self, tmp_path):
+    def test_excel_file_load(self, temp_data_dir):
         # Check that a valid excel file is read as a DataFrame
-        excel_file = tmp_path / "data.xlsx"
+        excel_file = temp_data_dir / "data.xlsx"
         self.data.to_excel(excel_file, index=False)
         assert df_from_file(excel_file).equals(self.data) is True
 
