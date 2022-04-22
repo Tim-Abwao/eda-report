b1da5e28ea8a3094795b16bee9d43d24306c0ff8
Tim-Abwao <abwaomusungu@gmail.com>
2022-04-22 18:26:34 +0300

    Bump version to 2.5.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ copyright = "2021, Abwao"
 author = "Abwao"
 
 # The full version, including alpha/beta/rc tags
-release = "2.4.1"
+release = "2.5.0"
 
 # -- General configuration ---------------------------------------------------
 
