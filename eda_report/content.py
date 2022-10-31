08edfbe3bb1fc52274506ab4bb0b2854b21074e6
Tim-Abwao <abwaomusungu@gmail.com>
2022-10-31 16:42:53 +0300

    Update _AnalysisResult
    Refactor analysis functions.
    Store results as dict for ease of access.

--- eda_report/content.py
+++ eda_report/content.py
@@ -61,21 +61,30 @@ class _AnalysisResult:
         self.GROUPBY_DATA = validate_groupby_data(
             data=self.multivariable.data, groupby_data=groupby_data
         )
+        self.analyzed_variables = self._analyze_variables()
         self.univariate_stats = self._get_univariate_statistics()
         self.contingency_tables = _get_contingency_tables(
-            self.multivariable.categorical_cols, self.GROUPBY_DATA
+            self.multivariable.data[
+                [
+                    col_name
+                    for col_name, var in self.analyzed_variables.items()
+                    if var.var_type != "numeric"
+                ]
+            ],
+            self.GROUPBY_DATA,
         )
+        self.normality_tests = self._test_for_normality()
         self.univariate_graphs = self._get_univariate_graphs()
         self.bivariate_graphs = _plot_multivariable(
             self.multivariable, color=graph_color
         )
+        self.bivariate_summaries = self._get_bivariate_summaries()
 
-    def _get_univariate_statistics(self) -> Dict:
-        """Compute summary statistics for all variables present.
+    def _analyze_variables(self) -> Dict[str, Variable]:
+        """Compute summary statistics and assess variable properties.
 
         Returns:
-            Dict[str, Union[_CategoricalStats, _DatetimeStats, _NumericStats]]:
-            Summary statistics.
+            Dict[str, Variable]: Univariate analysis results.
         """
         data = self.multivariable.data
         with Pool() as p:
@@ -93,6 +102,31 @@ class _AnalysisResult:
             )
         return univariate_stats
 
+    def _get_univariate_statistics(self) -> Dict[str, pd.DataFrame]:
+        """Get a dataframe of summary statistics for all variables.
+
+        Returns:
+            Dict[str, pandas.DataFrame]: Summary statistics.
+        """
+        return {
+            name: variable.summary_statistics._get_summary_statistics()
+            .to_frame()
+            .round(4)
+            for name, variable in self.analyzed_variables.items()
+        }
+
+    def _test_for_normality(self) -> Dict[str, pd.DataFrame]:
+        """Perform tests for normality.
+
+        Returns:
+            Dict[str, pandas.DataFrame]: Normality test results.
+        """
+        return {
+            name: variable.summary_statistics._test_for_normality()
+            for name, variable in self.analyzed_variables.items()
+            if variable.var_type == "numeric"
+        }
+
     def _get_univariate_graphs(self) -> Dict[str, Dict]:
         """Plot graphs for all variables present.
 
@@ -101,15 +135,15 @@ class _AnalysisResult:
         """
         with Pool() as p:
             variables_hue_and_color = [
-                (stat, self.GROUPBY_DATA, self.GRAPH_COLOR)
-                for stat in self.univariate_stats.values()
+                (variable, self.GROUPBY_DATA, self.GRAPH_COLOR)
+                for variable in self.analyzed_variables.values()
             ]
             univariate_graphs = dict(
                 tqdm(
                     # Plot variables in parallel processes
                     p.imap(_plot_variable, variables_hue_and_color),
                     # Progress-bar options
-                    total=len(self.univariate_stats),
+                    total=len(self.analyzed_variables),
                     bar_format=(
                         "{desc} {percentage:3.0f}%|{bar:35}| "
                         "{n_fmt}/{total_fmt}"
@@ -119,6 +153,34 @@ class _AnalysisResult:
                 )
             )
         return univariate_graphs
+
+    def _get_bivariate_summaries(self) -> Optional[Dict[str, str]]:
+        """Get descriptions of the nature of correlation between numeric
+        column pairs.
+
+        Returns:
+            Optional[Dict[str, str]]: Correlation info.
+        """
+        if hasattr(self.multivariable, "var_pairs"):
+            var_pairs = list(self.multivariable.var_pairs)
+
+            if len(self.multivariable.var_pairs) > 50:
+                # Take the top 50 var_pairs by magnitude of correlation.
+                var_pairs = (
+                    self.multivariable.correlation_df.unstack()[var_pairs]
+                    .sort_values(key=abs)
+                    .tail(50)
+                    .index
+                )
+            return {
+                var_pair: (
+                    f"{var_pair[0].title()} and {var_pair[1].title()} have "
+                    f"{self.multivariable.correlation_descriptions[var_pair]}."
+                )
+                for var_pair in var_pairs
+            }
+        else:
+            return None
 
 
 class _ReportContent(_AnalysisResult):
