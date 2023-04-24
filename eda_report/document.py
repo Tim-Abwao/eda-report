9ece8b045368d9755bcb7f9b6af826d519714bf7
Tim-Abwao <abwaomusungu@gmail.com>
2023-04-24 16:36:42 +0300

    Update document creation module
    Dynamically allocate image widths to ensure plots maintain aspect ratio
    and are of appropriate size.
    Replace _format_heading_spacing method with _format_paragraph_spacing.
    Strip trailing zeros from float values in tables.
    Get rid of redundant methods.
    Reduce cruft and improve readability.

--- eda_report/document.py
+++ eda_report/document.py
@@ -4,7 +4,7 @@ from typing import Iterable, Sequence, Union
 from docx import Document
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 from docx.shared import Inches, Pt
-from docx.text.parfmt import ParagraphFormat
+from docx.text.paragraph import Paragraph
 from pandas import DataFrame, Series
 
 from eda_report.content import _ReportContent
@@ -63,14 +63,8 @@ class ReportDocument(_ReportContent):
         )
         self.OUTPUT_FILENAME = output_filename
         self.TABLE_STYLE = table_style
-        self.document = Document()
-        self._get_report()
-
-    def _get_report(self) -> None:
-        """Calculate summary statistics, plot graphs, and save the results as
-        a .docx file.
-        """
-        self._create_title_page()
+        self.document = Document()  # Initialize report document
+        self._create_cover_page()
         self._get_univariate_analysis()
 
         if self.dataset._correlation_values is not None:
@@ -79,30 +73,13 @@ class ReportDocument(_ReportContent):
         self._to_file()
         logging.info(f"Done. Results saved as {self.OUTPUT_FILENAME!r}")
 
-    def _create_title_page(self) -> None:
+    def _create_cover_page(self) -> None:
         """Add a title and a brief summary of the data."""
         self.document.add_heading(self.TITLE, level=0)
-
         self.document.add_paragraph(self.intro_text)
-
         self._get_numeric_overview_table()
         self._get_categorical_overview_table()
         self.document.add_page_break()
-
-    def _format_heading_spacing(
-        self, format: ParagraphFormat, before: int = 15, after: int = 7
-    ) -> None:
-        """Set the spacing above or below a heading.
-
-        Args:
-            format (ParagraphFormat): The heading's format.
-            before (int, optional): Size of spacing above the heading in pt.
-                Defaults to 15.
-            after (int, optional): Size of spacing below the heading in pt.
-                Defaults to 7.
-        """
-        format.space_before = Pt(before)
-        format.space_after = Pt(after)
 
     def _get_numeric_overview_table(self) -> None:
         """Create a table with an overview of the numeric features present."""
@@ -112,8 +89,8 @@ class ReportDocument(_ReportContent):
             heading = self.document.add_heading(
                 "Overview of Numeric Features", level=1
             )
-            self._format_heading_spacing(heading.paragraph_format)
-            # count | mean | std | min | 25% | 50% | 75% | max
+            self._format_paragraph_spacing(heading)
+            # count | avg | stddev | min | 25% | 50% | 75% | max
             self._create_table(
                 data=self.dataset._numeric_stats,
                 header=True,
@@ -132,7 +109,7 @@ class ReportDocument(_ReportContent):
             heading = self.document.add_heading(
                 "Overview of Categorical Features", level=1
             )
-            self._format_heading_spacing(heading.paragraph_format)
+            self._format_paragraph_spacing(heading)
             # column-name | count | unique | top | freq | relative freq
             self._create_table(
                 data=self.dataset._categorical_stats,
@@ -149,9 +126,7 @@ class ReportDocument(_ReportContent):
         univariate_heading = self.document.add_heading(
             "1. Univariate Analysis", level=1
         )
-        self._format_heading_spacing(
-            univariate_heading.paragraph_format, before=0, after=0
-        )
+        self._format_paragraph_spacing(univariate_heading, before=0, after=0)
         for idx, variable in enumerate(self.variables.values(), start=1):
             var_name = variable.name
             description = self.variable_descriptions[var_name]
@@ -159,24 +134,22 @@ class ReportDocument(_ReportContent):
             graphs = self.univariate_graphs[var_name]
             contingency_table = self.contingency_tables.get(var_name)
             normality_tests = self.normality_tests.get(var_name)
-
+            # Variable's title and brief description
             heading = self.document.add_heading(
                 f"1.{idx} {var_name}".title(), level=2
             )
-            self._format_heading_spacing(
-                heading.paragraph_format, before=12, after=5
-            )
+            self._format_paragraph_spacing(heading, before=12, after=5)
             self.document.add_paragraph(description)
-
+            # Summary statistics table
             stats_heading = self.document.add_heading(
                 "Summary Statistics", level=4
             )
             stats_heading.alignment = WD_ALIGN_PARAGRAPH.CENTER
-
             self._create_table(summary_stats, column_widths=[2.5, 2])
-
-            for graph in (graphs).values():
-                self.document.add_picture(graph, width=Inches(4.4))
+            # Images of plotted graphs
+            for name, image in graphs.items():
+                width = 3.3 if name == "prob_plot" else 4.2
+                self.document.add_picture(image, width=Inches(width))
                 picture_paragraph = self.document.paragraphs[-1]
                 picture_paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
 
@@ -185,17 +158,14 @@ class ReportDocument(_ReportContent):
                     "Contingency table", level=4
                 )
                 contingency_table_heading.alignment = WD_ALIGN_PARAGRAPH.CENTER
-                # subtext = self.document.add_paragraph(
-                #     f"Index = '{var_name}' "
-                #     f"(missing: {variable.data.isna().sum()}). "
-                #     f"Columns = '{self.GROUPBY_DATA.name}' "
-                #     f"(missing: {self.GROUPBY_DATA.isna().sum()})"
-                # )
-                # subtext.alignment = WD_ALIGN_PARAGRAPH.CENTER
-                # subtext.runs[0].font.size = Pt(8)
-
+                context = self.document.add_paragraph(
+                    f"Index = '{var_name}', "
+                    f"Columns = '{self.GROUPBY_DATA.name}' "
+                )
+                context.alignment = WD_ALIGN_PARAGRAPH.CENTER
+                context.runs[0].font.size = Pt(8)
                 n_cols = contingency_table.shape[1]
-                max_width = 5 if n_cols > 5.2 else 3.2
+                max_width = 5.2 if n_cols > 5 else 3.2
                 col_width = max_width / n_cols
                 self._create_table(
                     data=contingency_table,
@@ -209,7 +179,6 @@ class ReportDocument(_ReportContent):
                     "Tests for Normality", level=4
                 )
                 norm_test_heading.alignment = WD_ALIGN_PARAGRAPH.CENTER
-
                 # type | p-value | conclusion
                 self._create_table(
                     data=normality_tests,
@@ -228,15 +197,12 @@ class ReportDocument(_ReportContent):
         bivariate_heading = self.document.add_heading(
             "2. Bivariate Analysis", level=1
         )
-        self._format_heading_spacing(
-            bivariate_heading.paragraph_format, before=0
-        )
-
+        self._format_paragraph_spacing(bivariate_heading, before=0)
         overview_heading = self.document.add_heading("2.1 Overview", level=2)
-        self._format_heading_spacing(overview_heading.paragraph_format)
+        self._format_paragraph_spacing(overview_heading)
         self.document.add_picture(
             self.bivariate_graphs["correlation_plot"],
-            width=Inches(6.7),
+            width=Inches(6.4),
         )
         picture_paragraph = self.document.paragraphs[-1]
         picture_paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
@@ -245,23 +211,34 @@ class ReportDocument(_ReportContent):
         pairwise_heading = self.document.add_heading(
             "2.2 Regression Plots (Top 20)", level=2
         )
-        self._format_heading_spacing(
-            pairwise_heading.paragraph_format, before=0
-        )
+        self._format_paragraph_spacing(pairwise_heading, before=0)
         for idx, var_pair in enumerate(self.bivariate_summaries, start=1):
             heading = self.document.add_heading(
                 f"2.2.{idx} {var_pair[0]} vs {var_pair[1]}".title(), level=3
             )
-            self._format_heading_spacing(
-                heading.paragraph_format, before=16, after=5
-            )
+            self._format_paragraph_spacing(heading, before=16, after=5)
             self.document.add_paragraph(self.bivariate_summaries[var_pair])
             self.document.add_picture(
                 self.bivariate_graphs["regression_plots"][var_pair],
-                width=Inches(3.5),
+                width=Inches(3.3),
             )
             picture_paragraph = self.document.paragraphs[-1]
             picture_paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+    def _format_paragraph_spacing(
+        self, paragraph: Paragraph, before: int = 15, after: int = 7
+    ) -> None:
+        """Set the spacing above or below a paragraph.
+
+        Args:
+            paragraph (Paragraph): A paragraph.
+            before (int, optional): Size of spacing above the paragraph in pt.
+                Defaults to 15.
+            after (int, optional): Size of spacing below the paragraph in pt.
+                Defaults to 7.
+        """
+        paragraph.paragraph_format.space_before = Pt(before)
+        paragraph.paragraph_format.space_after = Pt(after)
 
     def _create_table(
         self,
@@ -286,15 +263,12 @@ class ReportDocument(_ReportContent):
             header (bool, optional): Whether or not to include column names.
                 Defaults to False.
         """
-        n_cols = len(column_widths)
         table = self.document.add_table(
             rows=0,
-            cols=n_cols,
+            cols=len(column_widths),
             style=style or self.document.styles[self.TABLE_STYLE],
         )
         table.alignment = WD_ALIGN_PARAGRAPH.CENTER
-
-        # Set column dimensions
         for idx, width in enumerate(column_widths):
             table.columns[idx].width = Inches(width)
 
@@ -303,24 +277,23 @@ class ReportDocument(_ReportContent):
             header_labels = [""] + list(data.columns)
             for cell, value in zip(cells, header_labels):
                 cell.text = f"{value}"
-
                 # Font size and type-face have to be set at `run` level
                 run = cell.paragraphs[0].runs[0]
                 run.bold = True
                 run.font.size = Pt(font_size)
                 run.font.name = font_face
 
-        # Populate the rows
+        # Sequentially add and populate rows
         for row_data in data.itertuples():
             cells = table.add_row().cells
             for idx, (cell, value) in enumerate(zip(cells, row_data)):
-                text = f"{value}"
-                # Strip trailing zeros, if text is not all zeros.
-                if set(text) != {"0"}:
-                    text = text.rstrip("0").rstrip(".")
+                try:
+                    # Strip trailing zeros from float values
+                    text = f"{value:.4f}".rstrip("0").rstrip(".")
+                except ValueError:
+                    text = f"{value}"
 
                 cell.text = text
-
                 # Font size and type-face have to be set at `run` level
                 run = cell.paragraphs[0].runs[0]
                 run.font.size = Pt(font_size)
@@ -329,11 +302,11 @@ class ReportDocument(_ReportContent):
                 if idx == 0 and header:
                     run.bold = True
 
+        # Add empty paragraph. "Spacing" for docx Table isn't yet implemented
         self.document.add_paragraph()
 
     def _to_file(self) -> None:
         """Save the report as a file."""
-        # Set page margins
         for section in self.document.sections:
             section.left_margin = Inches(1.2)
             section.right_margin = Inches(1.2)
