497a4969bad7add5a65714697acb75a1a32a10cf
Tim-Abwao <abwaomusungu@gmail.com>
2023-08-19 23:38:14 +0300

    Bump version to 2.8.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ copyright = "2022, Abwao"
 author = "Abwao"
 
 # The full version, including alpha/beta/rc tags
-release = "2.8.0"
+release = "2.8.1"
 
 # -- General configuration ---------------------------------------------------
 
